bc3986ced053a42cb5cbef1f959d052cbf9b2f28
Enrique Lozano <61509169+enriqueloz88@users.noreply.github.com>
2022-06-11 15:06:04 +0200

    🍱 Add more content to the open project
    App currencies and default categories

---
+++ constants/app-currencies.ts
@@ -0,0 +1,236 @@
+import { Currency } from '../interfaces/currency.model';
+
+/** Array with all the avalaible currencies of the app. Each currency should have its flag in the assets folder */
+export const appCurrencies: Currency[] = [
+  {
+    code: 'EUR',
+    names: {
+      en: 'Euro',
+      es: 'Euro',
+    },
+  },
+  {
+    code: 'USD',
+    names: {
+      en: 'U.S. Dollar',
+      es: 'Dólar estadunidense',
+    },
+  },
+  {
+    code: 'JPY',
+    names: {
+      en: 'Yen',
+      es: 'Yen',
+    },
+  },
+  {
+    code: 'GBP',
+    names: {
+      en: 'Pound sterling',
+      es: 'Libra esterlina',
+    },
+  },
+  {
+    code: 'MXN',
+    names: {
+      en: 'Peso mexicano',
+      es: 'Mexican peso',
+    },
+  },
+  {
+    code: 'KRW',
+    names: {
+      en: 'South Korean Won',
+      es: 'Won surcoreano',
+    },
+  },
+  {
+    code: 'SEK',
+    names: {
+      en: 'Swedish Krona',
+      es: 'Corona sueca',
+    },
+  },
+  {
+    code: 'DKK',
+    names: {
+      en: 'Danish Krona',
+      es: 'Corona danesa',
+    },
+  },
+  {
+    code: 'PLN',
+    names: {
+      en: 'Zloty',
+      es: 'Zloty',
+    },
+  },
+  {
+    code: 'HUF',
+    names: {
+      en: 'Hugarian Forint',
+      es: 'Florín húngaro',
+    },
+  },
+  {
+    code: 'ALL',
+    names: {
+      en: 'Lek',
+      es: 'Lek',
+    },
+  },
+  {
+    code: 'AUD',
+    names: {
+      en: 'Australian Dollar',
+      es: 'Dólar australiano',
+    },
+  },
+  {
+    code: 'BRL',
+    names: {
+      en: 'Brazilian Real',
+      es: 'Real brasileño',
+    },
+  },
+  {
+    code: 'COP',
+    names: {
+      en: 'Colombian Peso',
+      es: 'Peso colombiano',
+    },
+  },
+  {
+    code: 'UYU',
+    names: {
+      en: 'Uruguayan Peso',
+      es: 'Peso uruguayo',
+    },
+  },
+  {
+    code: 'AOA',
+    names: {
+      en: 'Kwanza',
+      es: 'Kwanza',
+    },
+  },
+  {
+    code: 'ARS',
+    names: {
+      en: 'Argentine Peso',
+      es: 'Peso argentino',
+    },
+  },
+  {
+    code: 'CHF',
+    names: {
+      en: 'Swiss Franc',
+      es: 'Franco suizo',
+    },
+  },
+  {
+    code: 'RUB',
+    names: {
+      en: 'Russian Ruble',
+      es: 'Rublo',
+    },
+  },
+  {
+    code: 'CAD',
+    names: {
+      en: 'Canadian Dollar',
+      es: 'Dólar canadiense',
+    },
+  },
+  {
+    code: 'HKD',
+    names: {
+      en: 'Hong Kong Dollar',
+      es: 'Dólar de Hong Kong',
+    },
+  },
+  {
+    code: 'TRY',
+    names: {
+      en: 'Turkish Lira',
+      es: 'Lira turca',
+    },
+  },
+  {
+    code: 'EGP',
+    names: {
+      en: 'Egyptian pound',
+      es: 'Libra egipcia',
+    },
+  },
+  {
+    code: 'NGN',
+    names: {
+      en: 'Naira',
+      es: 'Naira',
+    },
+  },
+  {
+    code: 'ZAR',
+    names: {
+      en: 'Rand',
+      es: 'Rand',
+    },
+  },
+  {
+    code: 'FJD',
+    names: {
+      en: 'Fijian Dollar',
+      es: 'Dólar Fiyiano',
+    },
+  },
+  {
+    code: 'BGN',
+    names: {
+      en: 'Lev',
+      es: 'Leva',
+    },
+  },
+  {
+    code: 'KYD',
+    names: {
+      en: 'Cayman Island Dollar',
+      es: 'Dólar de las Islas Cayman',
+    },
+  },
+  {
+    code: 'NZD',
+    names: {
+      en: 'New Zeland Dollar',
+      es: 'Dólar de Nueva Zelanda',
+    },
+  },
+  {
+    code: 'CNH',
+    names: {
+      en: 'Yuan',
+      es: 'Yuan',
+    },
+  },
+  {
+    code: 'IMP',
+    names: {
+      en: 'Manx Pound',
+      es: 'Libra Manesa',
+    },
+  },
+  {
+    code: 'KGS',
+    names: {
+      en: 'Som',
+      es: 'Som',
+    },
+  },
+  {
+    code: 'OMR',
+    names: {
+      en: 'Omani rial',
+      es: 'Rial omaní',
+    },
+  },
+];
